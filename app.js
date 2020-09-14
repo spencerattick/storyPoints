148a85133f76cc766b80bc1a62efd6dfb36edcab
Spencer Attick <spencer.attick@spencer.attick>
2020-09-14 16:50:45 -0700

    add libraries point logic

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ let pointsOnTicket = 0;
 app.post("/storyPointsPost", function(req, res) {
   let properties = req.body.properties;
   res.sendStatus(200);
-  console.log('priority!!', properties.priority);
-  console.log('connection!!', properties.connection_mode);
-
 
 
   //priority
@@ -27,6 +24,7 @@ app.post("/storyPointsPost", function(req, res) {
     pointsOnTicket += priorityObj[properties.priority];
   }
 
+  console.log('priority: ', properties.priority);
 
   //connection mode
   let connectionModeObj = {
@@ -37,6 +35,45 @@ app.post("/storyPointsPost", function(req, res) {
   if (connectionModeObj.hasOwnProperty(properties.connection_mode)) {
     pointsOnTicket += connectionModeObj[properties.connection_mode];
   }
+
+  console.log('connection mode: ', properties.connection_mode);
+
+
+  //library
+  let libraryObj = {
+    'analytics.js': 1,
+    'analytics.js_consent_manager': 2,
+    'analytics.js_visual_tagger': 1,
+    'analytics-ios': 2,
+    'analytics-android': 2,
+    'both_ios_and_android': 2,
+    'analytics-react-native': 3,
+    'analytics-go': 2,
+    'analytics-java': 2.5,
+    'analytics.net': 2.5,
+    'analytics-node': 1,
+    'analytics-php': 2.5,
+    'analytics-python': 2.5,
+    'analytics-roku': 2.5,
+    'analytics-swift': 2.5,
+    'analytics-wordpress': 1,
+    'xid': 2.5,
+    'ajs-proxy': 2.5,
+    'analytics-xamarin': 2.5,
+    'analytics-ccp': 2,
+    'amp-analytics': 3,
+    'pixel-api': 2,
+    'personas_source': 2,
+    'no_specific_library': 0.5,
+    'http_api': 0.5
+  }
+
+  if (libraryObj.hasOwnProperty(properties.library)) {
+    pointsOnTicket += libraryObj[properties.library];
+  }
+
+  console.log('library: ', properties.library);
+
 
   console.log('priority points: ', pointsOnTicket);
 
